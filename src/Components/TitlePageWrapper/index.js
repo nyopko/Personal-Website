cd585b1e1849989a1185f5bf6f53d776abcc8132
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-10-08 16:34:21 -0400

    Added title page wrapper component, primed it for title page content when created

---
+++ src/Components/TitlePageWrapper/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function TitlePageWrapper() {
+  return (
+      <div className="TitlePageWrapper">
+         
+         </div>
+            );
+}
+
+export default TitlePageWrapper;
