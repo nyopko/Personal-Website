c233f69e68020a8766a9706e70a5005a06dbaca0
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-10-09 15:42:04 -0400

    Added grid to title page layout, included sample venn diagram image as placeholder before i build mine.

--- src/Components/TitlePageWrapper/index.js
+++ src/Components/TitlePageWrapper/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import Header from "../Header"
+import Header from "../Header";
+import Venn from "../Venn";
+
+// Grid 
+import Grid from '@material-ui/core/Grid';
 
 
 
 function TitlePageWrapper() {
-  return (
+   return (
       <div className="TitlePageWrapper">
+         <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+         >
          <Header />
-         </div>
-            );
+         <Venn />
+         </Grid>
+      </div>
+   );
 }
 
 export default TitlePageWrapper;
---
+++ src/Components/Venn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Venn() {
+    return (
+        <div id="vennDiagram">
+            <img src="https://images.template.net/wp-content/uploads/2015/12/07215928/Download-Triple-Venn-Diagram-for-Students-PDF-Format.png" alt="Venn Diagram" />
+
+        </div>
+
+    );
+}
+
+export default Venn;
