f9fa5a766281b93a0e31173c256e8c0b74bbc676
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-10-25 13:55:09 -0400

    Styled carosuel, testing mobile

--- src/Pages/coding.js
+++ src/Pages/coding.js
@@ -8,7 +8,14 @@ import { Carousel } from 'react-responsive-carousel';
 // Content import
 import SubPageHeader from '../Components/AboutHeader';
 import Nav from "../Components/Nav";
-import space from "../Components/img/space.png"
+
+// Image Import
+import space from "../Components/img/space.png"; 
+import planets from "../Components/img/planets.png"; 
+import search from "../Components/img/search.png";
+import results from "../Components/img/results.png";  
+import rover from "../Components/img/rover.png"; 
+import saturn from "../Components/img/saturn.png";
 
 
 class App extends Component {
@@ -16,53 +23,64 @@ class App extends Component {
         return (
             <div className="codingPage">
                 <Nav />
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <SubPageHeader />
+                    <h1 classname="pageHeaders">Coding</h1>
                     <Grid
                         container
-                        direction="column"
-                        justify="center"
+                        direction="row"
+                        justify="space-evenly"
                         alignItems="center"
                     >
-                        <SubPageHeader />
                         <div id="picCarousel">
-                        <Carousel>
-                <div>
-                    <img src={space} />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src={space} />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src={space} />
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
-            </div>
-                        <h1 classname="pageHeaders">Coding</h1>
-                        <Grid
-                            container
-                            direction="row"
-                            justify="space-evenly"
-                            alignItems="center"
-                        >
-                            <img src="https://via.placeholder.com/500x300" />
-                            <p className="codingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices orci vulputate, interdum eros ut, sodales arcu. Proin ac ex vitae libero commodo imperdiet in nec dui. Cras vitae mauris a sapien fringilla molestie. In hac habitasse platea dictumst. Pellentesque augue libero, feugiat sit amet sem nec, auctor commodo nunc. In elementum vitae felis nec placerat. Donec pulvinar, turpis non faucibus pellentesque, nisi leo venenatis lacus, vel placerat eros turpis eu felis. Ut ac risus arcu. Fusce porta iaculis felis, id cursus sapien mattis id. Proin congue sollicitudin sagittis. Nulla viverra finibus mauris, et luctus nunc. Suspendisse magna felis, malesuada id ex non, ullamcorper fermentum neque. Suspendisse fringilla quis velit eget semper.
+                        <Carousel showThumbs={false} swipeable={true}>
+                            <div>
+                                <img src={space} />
+                                <p className="legend">Space Education Homescreen</p>
+                            </div>
+                            <div>
+                                <img src={planets} />
+                                <p className="legend">Interactive Solar System Map</p>
+                            </div>
+                            <div>
+                                <img src={saturn} />
+                                <p className="legend">Planet Information from Map</p>
+                            </div>
+                            <div>
+                                <img src={rover} />
+                                <p className="legend">Mars Rover API Photo Page</p>
+                            </div>
+                            <div>
+                                <img src={search} />
+                                <p className="legend">Custom NASA Database Search Page</p>
+                            </div>
+                            <div>
+                                <img src={results} />
+                                <p className="legend">Example Search Results</p>
+                            </div>
+                        </Carousel>
+                    </div>
+                        <p className="codingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices orci vulputate, interdum eros ut, sodales arcu. Proin ac ex vitae libero commodo imperdiet in nec dui. Cras vitae mauris a sapien fringilla molestie. In hac habitasse platea dictumst. Pellentesque augue libero, feugiat sit amet sem nec, auctor commodo nunc. In elementum vitae felis nec placerat. Donec pulvinar, turpis non faucibus pellentesque, nisi leo venenatis lacus, vel placerat eros turpis eu felis. Ut ac risus arcu. Fusce porta iaculis felis, id cursus sapien mattis id. Proin congue sollicitudin sagittis. Nulla viverra finibus mauris, et luctus nunc. Suspendisse magna felis, malesuada id ex non, ullamcorper fermentum neque. Suspendisse fringilla quis velit eget semper.
 
 Sed pharetra facilisis magna, vitae volutpat tellus ultrices sit amet. Sed pretium orci quis ipsum tristique aliquet. Donec vitae arcu tortor. Curabitur at sapien in turpis porttitor fringilla eget condimentum mi. Etiam eget nunc quis libero mattis iaculis. Vivamus maximus euismod libero, ut convallis quam varius quis. Proin lectus est, pharetra non tempor eget, molestie quis tellus.</p>
-                            <img src="https://via.placeholder.com/500x300" />
-                            <p className="codingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices orci vulputate, interdum eros ut, sodales arcu. Proin ac ex vitae libero commodo imperdiet in nec dui. Cras vitae mauris a sapien fringilla molestie. In hac habitasse platea dictumst. Pellentesque augue libero, feugiat sit amet sem nec, auctor commodo nunc. In elementum vitae felis nec placerat. Donec pulvinar, turpis non faucibus pellentesque, nisi leo venenatis lacus, vel placerat eros turpis eu felis. Ut ac risus arcu. Fusce porta iaculis felis, id cursus sapien mattis id. Proin congue sollicitudin sagittis. Nulla viverra finibus mauris, et luctus nunc. Suspendisse magna felis, malesuada id ex non, ullamcorper fermentum neque. Suspendisse fringilla quis velit eget semper.
+                        <img src="https://via.placeholder.com/500x300" />
+                        <p className="codingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices orci vulputate, interdum eros ut, sodales arcu. Proin ac ex vitae libero commodo imperdiet in nec dui. Cras vitae mauris a sapien fringilla molestie. In hac habitasse platea dictumst. Pellentesque augue libero, feugiat sit amet sem nec, auctor commodo nunc. In elementum vitae felis nec placerat. Donec pulvinar, turpis non faucibus pellentesque, nisi leo venenatis lacus, vel placerat eros turpis eu felis. Ut ac risus arcu. Fusce porta iaculis felis, id cursus sapien mattis id. Proin congue sollicitudin sagittis. Nulla viverra finibus mauris, et luctus nunc. Suspendisse magna felis, malesuada id ex non, ullamcorper fermentum neque. Suspendisse fringilla quis velit eget semper.
 
 Sed pharetra facilisis magna, vitae volutpat tellus ultrices sit amet. Sed pretium orci quis ipsum tristique aliquet. Donec vitae arcu tortor. Curabitur at sapien in turpis porttitor fringilla eget condimentum mi. Etiam eget nunc quis libero mattis iaculis. Vivamus maximus euismod libero, ut convallis quam varius quis. Proin lectus est, pharetra non tempor eget, molestie quis tellus.</p>
 
-                            <img src="https://via.placeholder.com/500x300" />
-                            <p className="codingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices orci vulputate, interdum eros ut, sodales arcu. Proin ac ex vitae libero commodo imperdiet in nec dui. Cras vitae mauris a sapien fringilla molestie. In hac habitasse platea dictumst. Pellentesque augue libero, feugiat sit amet sem nec, auctor commodo nunc. In elementum vitae felis nec placerat. Donec pulvinar, turpis non faucibus pellentesque, nisi leo venenatis lacus, vel placerat eros turpis eu felis. Ut ac risus arcu. Fusce porta iaculis felis, id cursus sapien mattis id. Proin congue sollicitudin sagittis. Nulla viverra finibus mauris, et luctus nunc. Suspendisse magna felis, malesuada id ex non, ullamcorper fermentum neque. Suspendisse fringilla quis velit eget semper.
+                        <img src="https://via.placeholder.com/500x300" />
+                        <p className="codingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices orci vulputate, interdum eros ut, sodales arcu. Proin ac ex vitae libero commodo imperdiet in nec dui. Cras vitae mauris a sapien fringilla molestie. In hac habitasse platea dictumst. Pellentesque augue libero, feugiat sit amet sem nec, auctor commodo nunc. In elementum vitae felis nec placerat. Donec pulvinar, turpis non faucibus pellentesque, nisi leo venenatis lacus, vel placerat eros turpis eu felis. Ut ac risus arcu. Fusce porta iaculis felis, id cursus sapien mattis id. Proin congue sollicitudin sagittis. Nulla viverra finibus mauris, et luctus nunc. Suspendisse magna felis, malesuada id ex non, ullamcorper fermentum neque. Suspendisse fringilla quis velit eget semper.
 
 Sed pharetra facilisis magna, vitae volutpat tellus ultrices sit amet. Sed pretium orci quis ipsum tristique aliquet. Donec vitae arcu tortor. Curabitur at sapien in turpis porttitor fringilla eget condimentum mi. Etiam eget nunc quis libero mattis iaculis. Vivamus maximus euismod libero, ut convallis quam varius quis. Proin lectus est, pharetra non tempor eget, molestie quis tellus.</p>
 
-                        </Grid>
                     </Grid>
-                </div>
+                </Grid>
+            </div>
         );
     }
 }
